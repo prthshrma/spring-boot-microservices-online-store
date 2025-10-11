d2d838889359bce1e7b04e7057b96eecdf62814a
prthshrma <parthsharma.14.12@gmail.com>
2025-10-12 01:32:44 +0530

    Securing the services using KeyCloak.

---
+++ api-gateway/src/main/java/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain springSecurityWebFilterChain(ServerHttpSecurity serverHttpSecurity){
+        serverHttpSecurity.csrf(csrf -> csrf
+                .disable()
+                .authorizeExchange(exchange -> exchange
+                        .pathMatchers("/eureka/**")
+                        .permitAll()
+                        .anyExchange()
+                        .authenticated())
+                .oauth2ResourceServer(withDefaults()));   
+
+        return serverHttpSecurity.build();
+    }
+}
